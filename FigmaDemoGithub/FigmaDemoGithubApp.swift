f9cb5584b2dadba006961f7edaf23f5a98ffa49b
aminnzzz <amin.nazemzadeh@hatch.co>
2025-04-19 17:50:28 -0400

    gerard added

---
+++ FigmaDemoGithub/FigmaDemoGithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  FigmaDemoGithubApp.swift
+//  FigmaDemoGithub
+//
+//  Created by amin nazemzadeh on 4/17/25.
+//
+
+import SwiftUI
+
+@main
+struct FigmaDemoGithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FigmaDemoGithubTests/FigmaDemoGithubTests.swift
@@ -0,0 +1,17 @@
+//
+//  FigmaDemoGithubTests.swift
+//  FigmaDemoGithubTests
+//
+//  Created by amin nazemzadeh on 4/17/25.
+//
+
+import Testing
+@testable import FigmaDemoGithub
+
+struct FigmaDemoGithubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
