ebc61eaf32d716b90b1d523a89cf800dcf706844
aminnzzz <amin.nazemzadeh@hatch.co>
2025-05-07 21:34:58 -0400

    clean slate

--- FigmaDemoGithub/ContentView.swift
+++ FigmaDemoGithub/ContentView.swift
@@ -9,13 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            VStack(spacing: 20) {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .font(.system(size: 60))
+                    .foregroundStyle(.white)
+            }
+            .padding()
+            .background(.ultraThinMaterial)
+            .clipShape(RoundedRectangle(cornerRadius: 20))
+            .shadow(radius: 10)
         }
-        .padding()
     }
 }
 
--- FigmaDemoGithub/URLWatcher.swift
+++
@@ -1,8 +0,0 @@
-//
-//  URLWatcher.swift
-//  FigmaDemoGithub
-//
-//  Created by amin nazemzadeh on 5/1/25.
-//
-
-import Foundation
