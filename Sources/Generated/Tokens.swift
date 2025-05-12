ad3ede7bd653dd56ca8ad4aef2c765412afd86f1
aminnzzz <aminnzzz@users.noreply.github.com>
2025-05-12 18:24:26 +0000

    feat(design-tokens): update from Figma

---
+++ Sources/Generated/Tokens.swift
@@ -0,0 +1,15 @@
+// Tokens.swift — GENERATED FILE; DO NOT EDIT
+// Run `node generate-tokens-swift.js` to regenerate
+
+import Foundation
+
+public enum Tokens {
+    public enum Color: Double {
+    }
+    public enum Font: Double {
+    }
+    public enum Spacing: Double {
+    }
+    public enum Text: Double {
+    }
+}
